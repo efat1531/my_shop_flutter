959d536ab679af3895cff53facce4c7f51b3d748
Sp3cTer <20103033@iubat.edu>
2023-05-10 01:09:36 +0600

    Added Widget for render Product

---
+++ lib/widgets/products_item.dart
@@ -0,0 +1,50 @@
+// ignore_for_file: sort_child_properties_last, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class ProductItem extends StatelessWidget {
+  final String ProductTitle;
+  final String ProductImageUrl;
+  final int ProductPrice;
+  final String ProductId;
+
+  ProductItem(
+    this.ProductTitle,
+    this.ProductImageUrl,
+    this.ProductPrice,
+    this.ProductId,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.only(
+        topLeft: Radius.circular(8),
+        topRight: Radius.circular(8),
+      ),
+      child: GridTile(
+        child: Image.network(
+          ProductImageUrl,
+          fit: BoxFit.cover,
+        ),
+        footer: GridTileBar(
+          backgroundColor: Colors.black87,
+          leading: IconButton(
+            color: Theme.of(context).accentColor,
+            icon: Icon(Icons.favorite),
+            onPressed: () {},
+          ),
+          title: Text(
+            ProductTitle,
+            textAlign: TextAlign.center,
+          ),
+          trailing: IconButton(
+            color: Theme.of(context).accentColor,
+            icon: Icon(Icons.shopping_cart),
+            onPressed: () {},
+          ),
+        ),
+      ),
+    );
+  }
+}
